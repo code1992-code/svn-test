d13c18c3b4216788c5ce7a12952587d9d4a685e4
code1992-code <40210597+code1992-code@users.noreply.github.com>
2018-06-15 00:24:52 +0800

    3
    3

--- initialization.sh
+++ initialization.sh
@@ -11,9 +11,5 @@ if [ "$(ls -A $SPA)" ];then
 	svnserve -d -r /data/docker/svn && tail -f /etc/passwd
 else
 	svnadmin create $SPA
-	sed -i  's/# anon-access = read/anon-access = none/g' $SPA/conf/svnserve.conf  && sed -i  's/# password-db = passwd/password-db = passwd/g' $SPA/conf/svnserve.conf && sed -i  's/# auth-access = write/auth-
-
-access = write/g' $SPA/conf/svnserve.conf && sed -i  's/# realm = My First Repository/realm = web1/g' $SPA/conf/svnserve.conf && sed -i  's/# authz-db = authz/authz-db = authz/g' $SPA/conf/svnserve.conf && echo 
-
-linuxea=mark >> $SPA/conf/passwd && curl -Lks4  https://raw.githubusercontent.com/LinuxEA-Mark/docker-subversion1.9.4/master/authz >> $SPA/conf/authz && svnserve -d -r /data/docker/svn && tail -f /etc/passwd
+	sed -i  's/# anon-access = read/anon-access = none/g' $SPA/conf/svnserve.conf  && sed -i  's/# password-db = passwd/password-db = passwd/g' $SPA/conf/svnserve.conf && sed -i  's/# auth-access = write/auth-access = write/g' $SPA/conf/svnserve.conf && sed -i  's/# realm = My First Repository/realm = web1/g' $SPA/conf/svnserve.conf && sed -i  's/# authz-db = authz/authz-db = authz/g' $SPA/conf/svnserve.conf && echo linuxea=mark >> $SPA/conf/passwd && curl -Lks4  https://raw.githubusercontent.com/LinuxEA-Mark/docker-subversion1.9.4/master/authz >> $SPA/conf/authz && svnserve -d -r /data/docker/svn && tail -f /etc/passwd
 fi
