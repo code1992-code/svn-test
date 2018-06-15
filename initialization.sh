a1aef37a0b0d4e8da033350a0b680a5dc2874556
code1992-code <40210597+code1992-code@users.noreply.github.com>
2018-06-15 11:18:03 +0800

    2
    2

--- initialization.sh
+++ initialization.sh
@@ -11,5 +11,5 @@ if [ "$(ls -A $SPA)" ];then
 	svnserve -d -r /data/docker/svn && tail -f /etc/passwd
 else
 	svnadmin create $SPA
-	sed -i  's/# anon-access = read/anon-access = none/g' $SPA/conf/svnserve.conf  && sed -i  's/# password-db = passwd/password-db = passwd/g' $SPA/conf/svnserve.conf && sed -i  's/# auth-access = write/auth-access = write/g' $SPA/conf/svnserve.conf && sed -i  's/# realm = My First Repository/realm = weba/g' $SPA/conf/svnserve.conf && sed -i  's/# authz-db = authz/authz-db = authz/g' $SPA/conf/svnserve.conf && echo caokai=1 >> $SPA/conf/passwd && curl -Lks4 https://raw.githubusercontent.com/code1992-code/svn-test/master/authz >> $SPA/conf/authz && svnserve -d -r /data/docker/svn && tail -f /etc/passwd
+	sed -i  's/# anon-access = read/anon-access = none/g' $SPA/conf/svnserve.conf  && sed -i  's/# password-db = passwd/password-db = passwd/g' $SPA/conf/svnserve.conf && sed -i  's/# auth-access = write/auth-access = write/g' $SPA/conf/svnserve.conf && sed -i  's/# realm = My First Repository/realm = weba/g' $SPA/conf/svnserve.conf && sed -i  's/# authz-db = authz/authz-db = authz/g' $SPA/conf/svnserve.conf && echo caokai=2 >> $SPA/conf/passwd && curl -Lks4 https://raw.githubusercontent.com/code1992-code/svn-test/master/authz >> $SPA/conf/authz && svnserve -d -r /data/docker/svn && tail -f /etc/passwd
 fi
